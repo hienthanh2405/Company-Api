abccdbf481039faebfc5d15681fd0d96c6d66c3e
Hien Bui Thanh <ext.h.buithanh@achilles.com>
2018-12-26 07:52:08 +0700

    update unit test

---
+++ AssignmentCompany.Test/Controllers/CompanyControllerShould.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AssignmentCompany.Api.Controllers;
+using AssignmentCompany.Repo;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace AssignmentCompany.Test.Controllers
+{
+    public class CompanyControllerShould
+    {
+        [Fact]
+        public async Task Get_List_Company_Success()
+        {
+            //Arrange
+            var mockCompanyService = new Mock<IGenericRepository>();
+
+            var controller = new CompanyController(mockCompanyService.Object);
+
+            //Act
+            IActionResult result = await controller.GetListCompany();
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_List_Company_NotConnectDatabase_Fail()
+        {
+            //Arrage
+            var mockCompanyService = new Mock<IGenericRepository>();
+            var controller = new CompanyController(mockCompanyService.Object);
+
+            //Act
+            IActionResult result = await controller.GetListCompany();
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(400, okResult.StatusCode);
+        }
+    }
+}
---
+++ AssignmentCompany.Test/Services/CompanyServiceShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssignmentCompany.Data;
+
+namespace AssignmentCompany.Test.Services
+{
+    public class CompanyServiceShould
+    {
+        //var dataMock = new CompanyEntity()
+        //{
+        //    TitleCompany = "titleCompany",
+        //    NameCompany = "nameCompany",
+        //    GlobalId = Guid.NewGuid(),
+        //    DateCreated = DateTime.Now
+        //};
+    }
+}
